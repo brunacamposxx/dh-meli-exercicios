d0430307eab431ea120e202593145656d4263867
Bruna Campos <bruna.camposf@mercadolivre.com>
2022-10-11 11:28:08 -0300

    merge

--- modulo3/blog/src/main/java/com/dh/meli/blog/entity/Blog.java
+++ modulo3/blog/src/main/java/com/dh/meli/blog/entity/Blog.java
@@ -1,2 +1,23 @@
-package com.dh.meli.blog.entity;public class Blog {
+package com.dh.meli.blog.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Blog {
+    private Long id;
+    private String title;
+    private String author;
+    private String date;
+
+    public Blog(Blog blog) {
+        this.id = blog.getId();
+        this.title = blog.getTitle();
+        this.author = blog.getAuthor();
+        this.date = blog.getDate();
+    }
 }
--- modulo3/blog/src/main/java/com/dh/meli/blog/service/BlogService.java
+++ modulo3/blog/src/main/java/com/dh/meli/blog/service/BlogService.java
@@ -1,2 +1,38 @@
-package com.dh.meli.blog.service;public class BlogService {
+package com.dh.meli.blog.service;
+
+import com.dh.meli.blog.entity.Blog;
+import com.dh.meli.blog.exception.NotFoundException;
+import com.dh.meli.blog.repository.BlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class BlogService {
+    @Autowired
+    private BlogRepository blogRepository;
+
+    public void addBlog(Blog blog) {
+        blogRepository.addBlog(blog);
+    }
+    public Blog getBlogById(Long id) throws NotFoundException {
+        Optional<Blog> blog = blogRepository.getBlogById(id);
+
+        if (blog.isEmpty()) {
+            throw new NotFoundException("Blog not found");
+        }
+
+        return blog.get();
+
+    }
+
+    public List<Blog> getAll() {
+        return blogRepository.getAll().stream()
+                .map(Blog::new)
+                .collect(Collectors.toList());
+    }
+
 }
